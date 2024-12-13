efc0c01d1f5277af0d1942344127b9a4566b1d14
Divyesh Chitroda <div5yesh@gmail.com>
2024-12-12 23:58:04 -0800

    add template

---
+++ 2024/00/solution.py
@@ -0,0 +1,15 @@
+with open('sample.txt', 'r') as file:
+    content = file.read().split('\n')
+
+def solution(content):
+    result = 0
+
+    return result
+
+def solution2(content):
+    result = 0
+
+    return result
+
+print(solution(content))
+print(solution2(content))
